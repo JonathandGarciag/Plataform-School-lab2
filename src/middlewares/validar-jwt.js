1f4aef7f8c76fbcbc4bd38611fda1b88e7e0a63d
JonathandGarciag <Jgarcia-2023105@kinal.edu.gt>
2025-02-17 22:43:18 -0600

    Se termino los middlewares necesarios

--- src/middlewares/validar-jwt.js
+++ src/middlewares/validar-jwt.js
@@ -1,40 +1,40 @@
 import jwt from 'jsonwebtoken';
 import Usuario from '../users/user.model.js';
 
-export const validarJWT = async ( req, res,next ) => {
+export const validarJWT = async ( req, res, next ) => {
     const token = req.header("x-token");
 
     if (!token) {
         return res.status(401).json({
-            msg: "No hay token en la peticion"
-        })
+            msg: "No hay token en la petición"
+        });
     }
 
     try {
-        
         const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
         const usuario = await Usuario.findById(uid);
 
         if (!usuario) {
             return res.status(401).json({
-                msg: 'Usuario no existe en la base de datos'
-            })
+                msg: "Usuario no existe en la base de datos"
+            });
         }
 
         if (!usuario.status) {
             return res.status(401).json({
-                msg: 'Usuario no valido - usuarios con estado: false'
-            })
+                msg: "Usuario no válido - estado: false"
+            });
         }
 
-        req.usuario = usuario;
+        req.user = usuario; 
+        console.log("Usuario autenticado:", req.user); 
+
         next();
 
     } catch (e) {
         console.error(e);
         res.status(401).json({
-            msg: "Token no valido"
-        })
+            msg: "Token no válido"
+        });
     }
-}
-
+};
--- src/middlewares/validar-roles.js
+++ src/middlewares/validar-roles.js
@@ -19,5 +19,5 @@ export const tieneRole = (...roles) => {
         next();
     }
 
-    
 }
+
